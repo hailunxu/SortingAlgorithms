06cf330d9732873fb2424c3b40f59349aab854f6
hailunxu <50308565+hailunxu@users.noreply.github.com>
2020-08-08 22:58:05 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,135 @@
+/******************************************************************************
+
+Main.java
+
+
+ *******************************************************************************/
+
+package com.company;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
+
+public class Main  {
+
+    public static void main(String[] args) {
+
+        //using an arraylist here allows for quick adding and deleting of sorting threads since
+        //execute can only be used once per SortManager.
+        ArrayList<SortManager> managers = new ArrayList<SortManager>();
+
+        //gui stuff
+        JFrame frame = new JFrame("Sorting Algorithm Visualizer");
+        JPanel overPanel = new JPanel();
+        overPanel.setLayout(new BoxLayout(overPanel,BoxLayout.PAGE_AXIS ));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        JLabel arraySizeLabel = new JLabel("Array Size: ");
+        JLabel algorithmLabel = new JLabel("Algorithm: ");
+        JLabel delayLabel = new JLabel("Delay(ms): ");
+        JLabel resultsLabel = new JLabel( " Results: ");
+        JTextField arraySizeField = new JTextField("",1);
+        arraySizeField.setText("30");
+        JTextField delayField = new JTextField(1);
+        delayField.setText("50");
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(null);
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        JPanel algPanel = new JPanel(new BorderLayout());
+
+        //code for the drop down menu. right now this doesn't do anything.
+        //additional algorithms can be added to the string array.
+        String[] algMenuOptions = {"QuickSort", "SelectionSort", "BubbleSort", "ShellSort", "InsertionSort",};
+        JComboBox algMenu = new JComboBox(algMenuOptions);
+        algMenu.setSelectedIndex(0);
+        algMenu.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("sort index: " + algMenu.getSelectedIndex());
+                //algMenu.setSelectedIndex(e.);
+            }
+        });
+
+        //the generate button prepares all the essential elements.
+        JButton generateButton = new JButton( new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                for ( SortManager manager : managers) { // stop current sorting
+
+                    manager.cancel(true);
+                }
+
+                managers.clear();// delete the manager/sorting thread
+                SortManager sortManager = new SortManager(algMenu.getSelectedIndex()); //replace it with a new one
+                managers.add(sortManager);
+
+                int delay = 50; //default values if the user enters nothing
+                int size = 30;
+
+                if ( !arraySizeField.getText().trim().equals("") ) {  //filter out integer inputs for delay/# of elements.
+                    size = Integer.parseInt(arraySizeField.getText());
+                    if (Math.abs(size) > 40){// constrain to array size 40
+                        size = 40;
+                    }
+                }
+                if ( !delayField.getText().trim().equals("") ) {
+                    delay = Integer.parseInt(delayField.getText());
+                    if (Math.abs(delay) > 1000){// constrain to 1 second delay
+                        delay = 1000;
+                    }
+                }
+
+                sortManager.setArray(ArrayGenerator.generateArray(size)); // initialize everything
+                sortManager.setDelay(delay);
+                sortManager.drawBars();
+                sortManager.getUIPanel().setPreferredSize(new Dimension(800, 400));
+                frame.add(sortManager.getUIPanel(),BorderLayout.SOUTH);
+                frame.setVisible(true);
+
+            }
+        });
+        generateButton.setText("Generate!");
+
+        //button to begin the sort
+        JButton sortButton = new JButton( new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for ( SortManager manager : managers)
+                    manager.execute();
+            }
+        });
+        sortButton.setText("Sort!");
+
+        //here we add the gui elements onto the frmae.
+        //set up the subpanels
+        overPanel.setPreferredSize(new Dimension(800,200));
+        //input field
+        inputPanel.setPreferredSize(new Dimension(800,30));
+        arraySizeLabel.setBounds(200,10,70,20);
+        inputPanel.add(arraySizeLabel);
+        arraySizeField.setBounds(280,10,50,20);;
+        inputPanel.add(arraySizeField);
+        //delay field
+        inputPanel.add(delayLabel);
+        delayLabel.setBounds(400,10,70,20);
+        inputPanel.add(delayField);
+        delayField.setBounds(480,10,50,20);;
+        overPanel.add(inputPanel,BorderLayout.NORTH);
+        //buttons
+        buttonPanel.setPreferredSize(new Dimension(800,30));
+        buttonPanel.add(generateButton);
+        buttonPanel.add(sortButton);
+        overPanel.add(buttonPanel,FlowLayout.CENTER);
+        //algorithm select
+        algMenu.setPreferredSize(new Dimension(800,30));
+        algPanel.add(algorithmLabel, BorderLayout.WEST);
+        algPanel.add(algMenu);
+        overPanel.add(algPanel);
+
+        frame.add(overPanel,BorderLayout.NORTH);
+        frame.setVisible(true);
+    }
+}
---
+++ SortManager.java
@@ -0,0 +1,118 @@
+package com.company;
+
+import javax.swing.SwingWorker;
+import java.awt.Rectangle;
+
+public class SortManager extends SwingWorker {
+
+    private int counter = 0;
+    private final int barOffset = 100;
+    private final int barWidth = 5;
+    private final UIPanel uiPanel; // This is where all the UI elements will be. Also draws the sorting bars.
+    private int timesInitialized = 0;//this stops an exception from being thrown when the gui tries to draw an array before it's been created.
+    private String[] highlightedElements;//this determines what color each index will be colored.
+    Sortable algorithm; // current algorithm
+    private Rectangle[] bars; // holds the rectangular bars representing array elements.
+    private int[] array; // holds all values of the array.
+    private int delay = 100; // delay time in between each sorting step.
+
+    public SortManager( int algType ) {
+
+        uiPanel = new UIPanel(this);
+
+        switch ( algType ) {
+            case 0 : algorithm = new QuickSort(delay);
+                System.out.println("quicksort selected");
+                break;
+            case 1 : algorithm = new SelectionSort(delay);
+                System.out.println("selection sort selected");
+                break;
+            case 2 : algorithm = new BubbleSort(delay);
+                System.out.println("bubble sort selected");
+                break;
+            case 3 : algorithm = new ShellSort(delay);
+                System.out.println("shell sort selected");
+                break;
+            case 4 : algorithm = new InsertionSort(delay);
+                System.out.println("insertion sort selected");
+                break;
+        }
+    }
+
+    //call this method any time in your sort you want to swap
+    public void swapIndices(int i, int j, int[] arr, SortManager sortManager ) {
+
+        //both elements will be highlighted at once. remember to
+        //reset these bars to "" which will turn them black, after the sleeping the thread.
+        sortManager.highlightedElements[i] = "blue";
+        sortManager.highlightedElements[j] = "blue";
+
+        sortManager.getUIPanel().repaint();//redraw
+
+        //simultaneously swap the array values and their respective rectangles
+        int temp = arr[i];
+        sortManager.setBar(j, new Rectangle(j * 20 + barOffset, barOffset, barWidth, arr[i]));
+        arr[i] = arr[j];
+        sortManager.setBar(i, new Rectangle(i * 20 + barOffset, barOffset, barWidth, arr[j]));
+        arr[j] = temp;
+        System.out.println("indices " + i + " and " + j + " swapped.");
+    }
+
+    public void drawBars(){// this takes an array and draws all the corresponding bars onto the UI.
+        highlightedElements = new String[array.length];
+        bars = new Rectangle[ array.length ];
+        for ( int x = 0; x < array.length; x++) {
+            bars[x] = new Rectangle(x * 20 + barOffset ,barOffset, barWidth, array[x]);
+            highlightedElements[x] = "";
+
+            System.out.println("added bar to array at index "  + x);
+        }
+        uiPanel.repaint();
+        timesInitialized++;
+    }
+
+
+    @Override
+    protected Object doInBackground() { //kicks off a worker thread to begin the sorting.
+        algorithm.beginSort(array, this);
+        return null;
+    }
+
+    //counts the number of steps each algorithm takes and prints
+    public void stepCounter() {
+        counter++;
+        System.out.println("Number of steps is currently: " + counter);
+    }
+
+    //various accessor/mutators
+    public void setBar(int i, Rectangle rect){
+        bars[i] = rect;
+    }
+    public Rectangle getBar(int index){
+        return bars[index];
+    }
+    public Rectangle[] getBars(){
+        return bars;
+    }
+    public void setDelay(int delay) {
+        this.delay = delay;
+    }
+    public int getDelay(){
+        return delay;
+    }
+    public int[] getArray() {
+        return array;
+    }
+    public void setArray(int[] array){
+        this.array = array;
+    }
+    public UIPanel getUIPanel(){
+        return uiPanel;
+    }
+    public int getTimesInitialized(){
+        return timesInitialized;
+    }
+    public String[] highlightedElements(){ // stores highlight status of every box in the bar.
+        return highlightedElements;
+    }
+}
